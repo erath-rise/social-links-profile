7b13f16317427cff686f7550dbb55907014d7821
Lianne <ideng0212@gmail.com>
2024-08-26 00:06:02 +0100

    code

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='card'>
+        <img className='avatar' src="/public/avatar-jessica.jpeg" alt="#" />
+      <div className='info'>
+        <h1>Jessica Randall</h1>
+        <p className='address'>London, United Kingdom</p>
+      </div>
+      <p>&quot;Front-end developer and avid reader. &quot;</p>
+      <div className='links'>
+        <button>GitHub</button>
+        <button>Frontend Mentor</button>
+        <button>LinkedIn</button>
+        <button>Twitter</button>
+        <button>Instagram</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
