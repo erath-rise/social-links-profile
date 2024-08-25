047d7bc069fc121bdceb13bbd330a3ba7ce8a698
Lianne <ideng0212@gmail.com>
2024-08-26 00:19:48 +0100

    add avatar

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <div className='card'>
-        <img className='avatar' src="/public/avatar-jessica.jpeg" alt="#" />
+        <img className='avatar' src="/avatar-jessica.jpeg" alt="#" />
       <div className='info'>
         <h1>Jessica Randall</h1>
         <p className='address'>London, United Kingdom</p>
