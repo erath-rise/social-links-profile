381c2c40713394a400edc2c53480ab4ea089cbb8
Lianne <ideng0212@gmail.com>
2024-08-26 12:31:49 +0100

    modify css

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import './App.css'
 function App() {
 
   return (
-    <div className='card'>
-        <img className='avatar' src="/avatar-jessica.jpeg" alt="#" />
-      <div className='info'>
+    <div className='flex flex-col items-center justify-center gap-7 p-10 rounded-lg card'>
+        <img className='avatar mb-4' src="/avatar-jessica.jpeg" alt="#" />
+      <div className='info font-semibold'>
         <h1>Jessica Randall</h1>
         <p className='address'>London, United Kingdom</p>
       </div>
